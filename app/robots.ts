870b1549e5666e7056710edd6fceac5d3bece031
Adhar Limbu <limbunowan1234@gmail.com>
2026-07-13 19:48:53 +0530

    Fix robots.txt: allow public author profile pages to be crawled, only block exact /profile own-page

--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/admin', '/auth', '/bookmarks', '/profile', '/post'],
+        disallow: ['/admin', '/auth', '/bookmarks', '/profile$', '/post'],
       },
     ],
     sitemap: SITE + '/sitemap.xml',
