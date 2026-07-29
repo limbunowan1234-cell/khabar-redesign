b665bccebff0f1663b86cee97f13c370d68ac00c
Adhar Limbu <limbunowan1234@gmail.com>
2026-07-29 23:38:22 +0530

    Add khabardarjeeling.in to Next.js image remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-﻿/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'khabardarjeeling.in',
+        pathname: '/api/image-proxy**',
+      },
       {
         protocol: 'https',
         hostname: 'api.khabardarjeeling.in',
